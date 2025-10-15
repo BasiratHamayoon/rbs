55f196e3b44001fc7ca3718dfd4cca4f395cab7b
Basirat Hamayoon <basirathumayun@gmail.com>
2025-10-15 18:45:23 +0500

    Next.config configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
-    domains: ['your-image-domain.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'your-image-domain.com',
+      },
+    ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   experimental: {
     optimizeCss: true,
-  }
+  },
 };
 
 export default nextConfig;
